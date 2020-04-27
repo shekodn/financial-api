bcec986eee7dc88a4aba19ad6ecec45ad84687ee
shekodn <sdiazn19@gmail.com>
2020-04-27 11:36:20 -0500

    Add Transaction Model
    Also includes some tests

--- financial_app/models.py
+++ financial_app/models.py
@@ -13,3 +13,34 @@ class User(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.email} {self.age}"
+
+
+class Transaction(models.Model):
+    """
+    Transaction Model
+    Defines the attributes of a transaction
+    """
+    reference = models.CharField(max_length=30, unique=True)
+    account = models.CharField(max_length=30)
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10)
+    category = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="transactions"
+    )
+
+    def __str__(self):
+        return f"{self.reference} {self.account} {self.date} {self.amount} {self.type} {self.category} {self.user}"
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides default save method in order to make sure that:
+            - inflows are always >= 0
+            - outflows are always < 0
+        """
+        if self.amount >= 0:
+            self.type = "inflow"
+        else:
+            self.type = "outflow"
+        super(Transaction, self).save(*args, **kwargs)
--- financial_app/tests/test_models.py
+++ financial_app/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import User
+from ..models import User, Transaction
 from django.db.utils import IntegrityError
 from django.db import transaction
 
@@ -27,3 +27,71 @@ class UserTestCase(TestCase):
             pass
 
         self.assertEquals(User.objects.count(), 1)
+
+
+class TransactionTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(name="user", age=18, email="user@example.com")
+        self.tx1 = Transaction.objects.create(
+            reference="001",
+            account="S00099",
+            date="2020-01-13",
+            amount=100.21,
+            type="inflow",
+            category="salary",
+            user=self.user,
+        )
+
+    def test_transaction(self):
+        self.assertEquals(Transaction.objects.count(), 1)
+
+    def test_transaction_reference_should_be_unique(self):
+        self.assertEquals(Transaction.objects.count(), 1)
+
+        try:
+            with transaction.atomic():
+                Transaction.objects.create(
+                    reference=self.tx1.reference,
+                    account="S00099",
+                    date="2020-01-13",
+                    amount=100.21,
+                    type="inflow",
+                    category="salary",
+                    user=self.user,
+                )
+                self.fail("Duplicate reference allowed")
+        except IntegrityError:
+            pass
+
+        self.assertEquals(User.objects.count(), 1)
+
+    def test_transaction_type(self):
+        """
+        Make sure that the type is linked to the amount.
+            - inflow is always >= 0
+            - outflow is always < 0
+        """
+
+        self.tx = Transaction.objects.create(
+            reference="002",
+            account="S00099",
+            date="2020-01-13",
+            amount=-42,
+            type="this makes no sense",
+            category="salary",
+            user=self.user,
+        )
+
+        self.assertEquals(self.tx.type, "outflow")
+
+        self.tx = Transaction.objects.create(
+            reference="003",
+            account="S00099",
+            date="2020-01-13",
+            amount=42,
+            type="this makes no sense (again)",
+            category="salary",
+            user=self.user,
+        )
+
+        self.assertEquals(self.tx.type, "inflow")
